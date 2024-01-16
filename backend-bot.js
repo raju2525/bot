fec559ba60b89842f6c7ad47ed166d02f65be68f
RAJUReDDY <69119292+raju2525@users.noreply.github.com>
2024-01-16 21:42:30 +0530

    Update backend-bot.js

--- backend-bot.js
+++ backend-bot.js
@@ -78,7 +78,7 @@ async function checkBalance() {
   try {
     const walletAddress = await wallet.getAddress();
     const balance = await contract.balanceOf(walletAddress);
-    const balanceInEther = utils.formatUnits(balance, 6);
+    const balanceInEther = ethers.utils.formatEther(balance);
     consoleAsStatus(`BALANCE: ${balanceInEther}`);
   } catch (error) {
     consoleAsStatus('Error checking balance:', error);
